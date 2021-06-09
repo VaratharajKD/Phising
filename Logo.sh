546f7f6dd8aaee74943a34f8e5def95e064abc48
varatharaj <83505184+VaratharajKD@users.noreply.github.com>
2021-06-09 00:39:27 -0700

    First commit

---
+++ Logo.sh
@@ -0,0 +1,27 @@
+﻿#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██   █  ██   ██ ██ ██        \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[1;32m ██████  ███████ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
+  echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL BY MANI \e[0m''\e[1;37m\e[0m\e[1;37m  \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/Tech-rockerr/AdvPhishing
+Any help collabs on LinkedIn - VARATHARAJ S \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
+      echo ""                                                
+      }
+      banner
